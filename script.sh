7d0856c31742e394cc5ed1d70885d3b2517ebfab
ntwrkr <83679066+ntwrkrcrt@users.noreply.github.com>
2022-10-24 15:05:14 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,14 +3,10 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu -y
 
-cd $HOME
-ver="1.18.3"
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
 
 cd $HOME
 rm -rf celestia-node
